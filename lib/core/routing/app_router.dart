992360e2531070fb877d962cb4faad31e33ddebe
DESKTOP-JOQGR9N\original <alitolpa888@gmail.com>
2024-03-30 15:22:09 +0200

    appRoutes

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:docdoc_app/core/routing/routes.dart';
+import 'package:docdoc_app/features/login/ui/widget/login_screen.dart';
+import 'package:docdoc_app/features/onboarding/onboarding_screen.dart';
+
+class AppRouter {
+  Route generateRoutes(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.onboardingScreen:
+        return MaterialPageRoute(builder: (_) => OnboardingScreen());
+      case Routes.loginScreen:
+        return MaterialPageRoute(builder: (_) => LoginScreen());
+      default:
+        return MaterialPageRoute(
+            builder: (_) => Scaffold(
+                  body: Center(
+                    child:
+                        Text('No route defined as ${settings.name}'),
+                  ),
+                ));
+    }
+  }
+}
---
+++ lib/doc_app.dart
@@ -0,0 +1,14 @@
+import 'package:docdoc_app/core/routing/app_router.dart';
+import 'package:flutter/material.dart';
+
+class DocApp extends StatelessWidget{
+   final AppRouter appRouter;
+
+  const DocApp({super.key, required this.appRouter});
+  @override
+  Widget build(BuildContext context) {
+    return
+        const Scaffold();
+  }
+
+}
