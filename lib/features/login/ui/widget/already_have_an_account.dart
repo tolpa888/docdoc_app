65dff6a9c604127c871cc96e4db809908e2bab89
DESKTOP-JOQGR9N\original <alitolpa888@gmail.com>
2024-04-21 22:32:11 +0200

    bugs/align text in center

--- lib/features/login/ui/widget/already_have_an_account.dart
+++ lib/features/login/ui/widget/already_have_an_account.dart
@@ -1,25 +1,20 @@
 import 'package:docdoc_app/core/theming/styles.dart';
 import 'package:flutter/cupertino.dart';
 
-class AlreadyHaveAnAccount extends StatelessWidget{
+class AlreadyHaveAnAccount extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-return
-    RichText(
-textAlign: TextAlign.center,
-      text: TextSpan(
-        children: [
+    return Align(
+      alignment: Alignment.center,
+      child: RichText(
+        textAlign: TextAlign.center,
+        text: TextSpan(children: [
           TextSpan(
-            text: 'Already have an account yet? ',
-            style: Styles.font13spBlackRegular
-          ),
-          TextSpan(
-            text: 'Sign Up',
-            style: Styles.font13spMainBlueSemiBold
-          ),
-        ]
+              text: 'Already have an account yet? ',
+              style: Styles.font13spBlackRegular),
+          TextSpan(text: 'Sign Up', style: Styles.font13spMainBlueSemiBold),
+        ]),
       ),
     );
   }
-
 }
